ab014a83e266d871fd7694c486b31f64a6062b53
Cameron <camerondev343@gmail.com>
2020-12-08 12:39:47 -0500

    Init commit, added convenience message, added elip

---
+++ Server.scala
@@ -0,0 +1,16 @@
+import java.net._
+import java.io._
+
+object Server extends App {
+    val server =  new ServerSocket(8080)
+
+    while (true) {
+        var socket = server.accept()
+        var input = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        var line = ""
+        System.out.println("Starting server...")
+        while ((line = input.readLine()) != null) {
+            System.out.println(line)
+        }
+    }
+}
