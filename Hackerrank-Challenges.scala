0de6176a1fdc77e14ea07b96637a87686390c7b6
Cameron <camerondev343@gmail.com>
2020-12-08 14:33:01 -0500

    Added CompareTripletsSolution object and imports

--- Hackerrank-Challenges.scala
+++ Hackerrank-Challenges.scala
@@ -1,4 +1,20 @@
 import java.io.PrintWriter
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+import scala.collection._
+import scala.concurrent._
+import scala.io._
+import scala.math._
+import scala.sys._
+import scala.util.matching._
+import scala.reflect._
 
 object simpleArraySumSolution {
 
@@ -25,6 +41,36 @@ object simpleArraySumSolution {
         val result = simpleArraySum(ar)
 
         printWriter.println(result)
+
+        printWriter.close()
+    }
+}
+
+object CompareTripletsSolution {
+
+    // Complete the compareTriplets function below.
+    def compareTriplets(a: Array[Int], b: Array[Int]): Array[Int] = {
+        val sol:Array[Int] = Array(0, 0)
+        
+        for ( i <- 0 to 2) {
+            if (a(i) > b(i))
+                sol(0)+=1
+            else if (a(i) < b(i))
+                sol(1)+=1
+        }
+        sol
+
+    }
+
+    def main(args: Array[String]) {
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val a = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+        val b = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+        val result = compareTriplets(a, b)
+
+        printWriter.println(result.mkString(" "))
 
         printWriter.close()
     }
